c2c8ed38df2c08e8b24dabf66066a41869d79e52
elgamerjugon <josue.molina0593@gmail.com>
2024-02-10 12:14:24 -0600

    Third day of programming, created the pipeline to transform data using preprocessor

--- housing_prices.py
+++ housing_prices.py
@@ -93,3 +93,213 @@ line_y = intercept + slope * line_x
 line_data = go.Scatter(x = line_x, y = line_y, mode= "lines", name = "Normal Line", line = dict(color = "green"))
 
 qq_fig.add_trace(line_data)
+
+# Distribution of dwelling types and their relation to sale prices
+
+dwelling_types = df.BldgType.value_counts()
+dwelling_prices = df.groupby("BldgType")["SalePrice"].mean()
+formatted_dwelling_prices = ["$" + f"{value: ,.2f}" for value in dwelling_prices.values]
+
+# Bar charts
+fig1 = go.Figure(data=[go.Bar(
+    x=dwelling_types.index,
+    y=dwelling_types.values,
+    marker_color='rgb(76, 175, 80)',
+    text=dwelling_types.values,
+    textposition='outside',
+    width=0.4,
+    marker=dict(line=dict(width=2, color='rgba(0,0,0,1)'), opacity=1)
+)])
+fig1.update_layout(
+    title='Distribution of Building Types',
+    xaxis_title='Building Type',
+    yaxis_title='Count',
+    plot_bgcolor='rgba(34, 34, 34, 1)',
+    paper_bgcolor='rgba(34, 34, 34, 1)',
+    font=dict(color='white')
+)
+
+fig2 = go.Figure(data=[go.Bar(
+    x=dwelling_prices.index,
+    y=dwelling_prices.values,
+    marker_color='rgb(156, 39, 176)',
+    text=formatted_dwelling_prices,
+    textposition='outside',
+    width=0.4,
+    marker=dict(line=dict(width=2, color='rgba(0,0,0,1)'), opacity=1)
+)])
+
+fig2.update_layout(
+    title='Average Sale Price by Building Type',
+    xaxis_title='Building Type',
+    yaxis_title='Price',
+    plot_bgcolor='rgba(34, 34, 34, 1)',
+    paper_bgcolor='rgba(34, 34, 34, 1)',
+    font=dict(color='white')
+)
+
+# There are more 1 fam building types than any other types
+# The sale price seems to be related with the building type
+
+# Street and alley access types effect on sales price
+street_prices = df.groupby("Street")["SalePrice"].mean()
+alley_prices = df.groupby("Alley")["SalePrice"].mean()
+
+# What this function does is like an if statement, if index == Pave, then the 
+# value is purple, if not the the valie is green
+colors_street = np.where(street_prices.index == "Pave", "purple", "green")
+
+fig5 = px.bar(x=street_prices.index, y=street_prices.values, title='Average Sale Price by Street Type',
+              template='plotly_dark', text=street_prices.values,
+              color=colors_street, color_discrete_sequence=['purple', 'green'])
+
+fig5.update_traces(texttemplate='$%{text:,.0f}', textposition='outside')
+fig5.update_yaxes(title='Sale Price', tickprefix='$', tickformat=',')
+fig5.update_xaxes(title='Street Type')
+fig5.update_layout(showlegend=False)
+
+# Alley Prices
+colors_alley = np.where(alley_prices.index == 'Pave', 'purple', 'green')
+fig6 = px.bar(x=alley_prices.index, y=alley_prices.values, title='Average Sale Price by Alley Type',
+              template='plotly_dark', text=alley_prices.values,
+              color=colors_alley, color_discrete_sequence=['purple', 'green'])
+
+fig6.update_traces(texttemplate='$%{text:,.0f}', textposition='outside')
+fig6.update_yaxes(title='Sale Price', tickprefix='$', tickformat=',')
+fig6.update_xaxes(title='Alley Type')
+fig6.update_layout(showlegend=False)
+
+# Average sale price by propery shape
+colors = px.colors.qualitative.Plotly
+
+shape_prices = df.groupby("LotShape")["SalePrice"].mean()
+contour_prices = df.groupby("LandContour")["SalePrice"].mean()
+
+# Shape Prices
+fig7 = px.bar(x=shape_prices.index, y=shape_prices.values, title='Average Sale Price by Property Shape',
+              template='plotly_dark', text=shape_prices.values)
+
+fig7.update_traces(marker_color=colors, texttemplate='$%{text:,.0f}', textposition='outside')
+fig7.update_yaxes(title='Sale Price', tickprefix='$', tickformat=',')
+fig7.update_xaxes(title='Property Shape')
+fig7.update_layout(showlegend=False)
+
+# Contour Prices
+fig8 = px.bar(x=contour_prices.index, y=contour_prices.values, title='Average Sale Price by Property Contour',
+              template='plotly_dark', text=contour_prices.values)
+
+fig8.update_traces(marker_color=colors, texttemplate='$%{text:,.0f}', textposition='outside')
+fig8.update_yaxes(title='Sale Price', tickprefix='$', tickformat=',')
+fig8.update_xaxes(title='Property Contour')
+fig8.update_layout(showlegend=False)
+
+# It also looks like there's some relation between
+# the contour, land shape and the price
+
+# Calculating property age
+df["PropertyAge"] = df.YrSold - df.YearBuilt
+
+# Creating a graph to see for correlation between variables
+# of propery age and Sale Price
+
+age_price_corr = df.PropertyAge.corr(df["SalePrice"])
+age_price_corr
+
+# Visualize the correlation
+fig9 = px.scatter(df, x = "PropertyAge", y = "SalePrice",
+                  title = "Property Age vs Sale Price", color = "PropertyAge",
+                  color_continuous_scale = px.colors.sequential.Purp)
+
+fig9.update_layout(plot_bgcolor = "rgb(30, 30, 30)", paper_bgcolor = "rgb(30, 30, 30)", font = dict(color = "white"))
+fig9.show()
+
+# The correlation coeficient is kind of strong, it has a 
+# negative effect, while the property age is higher, the price
+# tend to be lower
+
+# Calculating the correlation between living area and Sale Price
+
+living_area_corr = df.GrLivArea.corr(df.SalePrice)
+living_area_corr
+
+# The coeficient is higher with a positive impact
+
+fig10 = px.scatter(df, x = "GrLivArea", y = "SalePrice",
+                  title = "GrLivArea vs Sale Price", color = "GrLivArea",
+                  color_continuous_scale = px.colors.sequential.Purp)
+
+fig10.update_layout(plot_bgcolor = "rgb(30, 30, 30)", paper_bgcolor = "rgb(30, 30, 30)", font = dict(color = "white"))
+fig10.show()
+
+# Start with boxplots
+yearly_avg_sale_price = df.groupby("YrSold")["SalePrice"].mean()
+
+fig13 = px.box(df, x = "YrSold", y = "SalePrice", 
+               title = "Sale Price Trend Over the Years",
+               points = False, color_discrete_sequence = ["green"])
+
+fig13.add_trace(px.line(x = yearly_avg_sale_price.index, y = yearly_avg_sale_price).data[0])
+
+display(HTML(create_scrollable_table(pd.DataFrame(df[df.YrSold == 2007].SalePrice.sort_values()), "2007-sales", "2007 Sales")))
+
+fig13.update_traces(line = dict(color = "purple", width = 4), selector = dict(type = "scatter", mode = "lines"))
+
+for year, avg_price in yearly_avg_sale_price.items():
+    fig13.add_annotation(
+        x = year,
+        y = avg_price,
+        text = f"{avg_price:,.0f}",
+        font = dict(color = "white"),
+        showarrow = False,
+        bgcolor = "rgba(128, 0, 128, 0.6)"
+    )
+
+fig13.update_layout(
+    plot_bgcolor = "rgb(30, 30, 30)",
+    paper_bgcolor = "rgb(30, 30, 30, 30)",
+    font = dict(color = "white"),
+    xaxis_title = "Year Sold",
+    yaxis_title = "Sale Price"
+)
+
+# Creating pipelines with sklearn
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+
+# Define transformers for numerical and categorical columns
+numerical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean')),
+    ('scaler', StandardScaler())
+])
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore', sparse = False))
+])
+# Update categorical and numerical columns
+categorical_columns = df.select_dtypes(include=['object', 'category']).columns
+numerical_columns = df.select_dtypes(include=['int64', 'float64']).columns
+
+# Remove target variable from numerical columns
+# As we are starting with the model itself, let's drop
+# the dependant variable
+numerical_columns = numerical_columns.drop('SalePrice')
+
+# Combine transformers using ColumnTransformer
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_columns),
+        ('cat', categorical_transformer, categorical_columns)
+    ],remainder = 'passthrough') # this value is used to specify what will happen to remaining columns not specified on transformer, you can drop them or pass them through
+
+# Create a pipeline with the preprocessor
+pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor)])
+
+# Apply the pipeline to your dataset
+X = df.drop('SalePrice', axis=1)
+y = np.log(df['SalePrice']) # Apply a normalization, remember to denormalize when predicting values
+X_preprocessed = pipeline.fit_transform(X)
