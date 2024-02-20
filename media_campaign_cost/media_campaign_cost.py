019763d9d142404d4aeb9103cd6c7520b928d679
elgamerjugon <josue.molina0593@gmail.com>
2024-02-19 22:17:52 -0600

    Created Housing prices 2

--- media_campaign_cost/media_campaign_cost.py
+++ media_campaign_cost/media_campaign_cost.py
@@ -4,6 +4,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import scipy.stats as stats
 from  IPython.display import display, HTML
+import matplotlib.pyplot as plt
 
 def create_scrollable_table(df, table_id, title):
     html = f'<h3>{title}</h3>'
@@ -40,6 +41,59 @@ df.columns
 df.rename(columns = {
     "store_sales(in millions)": "store_sales", 
     "unit_sales(in millions)": "unit_sales", 
-    "avg_cars_at_home(aprox.)1": "avg_cars_at_home"
+    "avg_cars_at home(approx).1": "avg_cars_at_home"
     }, inplace = True)
 
+df.hist(bins = 50, figsize = (15, 8))
+plt.show
+
+hist = go.Histogram(x = df.store_sales, name = "Histogram", histnorm = "probability density")
+continuous_variables_df = df[["store_sales", "unit_sales", "total_children", "num_children_at_home", "avg_cars_at_home", "gross_weight", "cost"]]
+fig = px.scatter_matrix(
+    continuous_variables_df,
+    title="Scatter Matrix of Continuous Variables",
+    dimensions=["store_sales", "unit_sales", "total_children", "num_children_at_home", "avg_cars_at_home", "gross_weight", "cost"],
+    # color="store_sales",  # Customize color based on a variable (optional)
+    # symbol="store_sales",  # Customize marker symbol based on a variable (optional)
+    labels={col: col.replace("_", " ").title() for col in continuous_variables_df.columns}  # Custom axis labels
+)
+
+fig.update_layout(
+    autosize=False,
+    width=800,
+    height=800,
+    margin=dict(l=50, r=50, b=50, t=50),
+)
+fig.show()
+
+
+
+
+
+
+
+
+
+
+
+
+# Create the scatter matrix plot
+fig = px.scatter_matrix(
+    continuous_variables_df,
+    title="Scatter Matrix of Continuous Variables",
+    dimensions=["store_sales", "unit_sales", "total_children", "num_children_at_home", "avg_cars_at_home", "gross_weight", "cost"],
+    # color="store_sales",  # Customize color based on a variable (optional)
+    symbol="store_sales",  # Customize marker symbol based on a variable (optional)
+    labels={col: col.replace("_", " ").title() for col in continuous_variables_df.columns}  # Custom axis labels
+)
+
+# Adjust subplot size and layout
+fig.update_layout(
+    autosize=False,
+    width=1000,
+    height=1000,
+    margin=dict(l=50, r=50, b=50, t=50),
+)
+
+# Show the plot
+fig.show()
